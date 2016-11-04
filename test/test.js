63e150d5470eac74aa27cdee40daf6e66020e450
Kolja <kolja.kirchner@datatrans.ch>
2016-11-04 14:46:16 +0100

    add inflate()

--- test/test.js
+++ test/test.js
@@ -1,13 +1,25 @@
-const mocha = require('mocha')
 const assert = require('assert')
 const fl = require('../lib')
 const data = require('./data')
+const fp = require('lodash/fp')
 
 describe('flatulence', () => {
-  describe('#flatten()', () => {
+  
+  describe('#deflate()', () => {
     it('should flatten object', () => {
-      const flattened = fl.flatten(data)
-      console.log(flattened)
+      const flattened = fl.deflate(data)
+      
+      assert.equal(flattened['array[0][1]'], 1)
+    })
+  })
+
+  describe('#inflate()', () => {
+    it('should reverse flattened object to an inflated one', () => {
+      const flattened = fl.deflate(data)
+      const inflated = fl.inflate(flattened, 'cc')
+
+      assert.deepEqual(data.cc, inflated)
+
     })
   })
 })
