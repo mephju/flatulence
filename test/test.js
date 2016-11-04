4a2b53d7271e71f8cc92959b48c26e964bfec9d2
Kolja <kolja.kirchner@datatrans.ch>
2016-11-04 17:46:43 +0100

    fix prepending of custom prefix when deflating object

--- test/test.js
+++ test/test.js
@@ -1,15 +1,33 @@
 const assert = require('assert')
-const fl = require('../lib')
-const data = require('./data')
-const fp = require('lodash/fp')
+const fl     = require('../lib')
+const data   = require('./data')
+const fp     = require('lodash/fp')
 
 describe('flatulence', () => {
   
   describe('#deflate()', () => {
     it('should flatten object', () => {
       const flattened = fl.deflate(data)
+
+      console.log(flattened)
       
       assert.equal(flattened['array[0][1]'], 1)
+    })
+
+    it('should use prefix arg if provided', () => {
+      const prefix = 'this.is.my.custom.prefix'
+      const flattened = fl.deflate(
+        { 
+          id: 'anid', 
+          map: { a:1, b:2, c:3} 
+        }, 
+        prefix
+      )
+
+      console.log(flattened)
+      
+      assert.equal(flattened[prefix + '.id'], 'anid')
+      assert.equal(flattened[prefix + '.map.a'], 1)
     })
   })
 
